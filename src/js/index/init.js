39f1867df2f44350e2ceffad01f54ff032ef1663
tylerj <tylerjcagle@gmail.com>
2022-08-16 11:47:20 -0700

    updated registration

--- src/js/index/init.js
+++ src/js/index/init.js
@@ -1,29 +1,38 @@
-const THREE = require('three');
-const { debounce } = require('@ykob/js-util');
+const THREE = require("three");
+const { debounce } = require("@ykob/js-util");
 
-const SmoothScrollManager = require('../smooth_scroll_manager/SmoothScrollManager').default;
-const TitleObject = require('./TitleObject').default;
-const BuildingObject = require('./BuildingObject').default;
+const SmoothScrollManager =
+  require("../smooth_scroll_manager/SmoothScrollManager").default;
+const TitleObject = require("./TitleObject").default;
+const BuildingObject = require("./BuildingObject").default;
 // const SkyOctahedron = require('./SkyOctahedron').default;
 // const SkyOctahedronShell = require('./SkyOctahedronShell').default;
-const Ground = require('./Ground').default;
-const Debris = require('./Debris').default;
-const PostEffect = require('./PostEffect').default;
-const Typo = require('../sketch/easy_glitch/Typo').default
+const Ground = require("./Ground").default;
+const Debris = require("./Debris").default;
+const PostEffect = require("./PostEffect").default;
+const Typo = require("../sketch/easy_glitch/Typo").default;
 
-export default function() {
+export default function () {
   const scrollManager = new SmoothScrollManager();
 
-  const canvas = document.getElementById('canvas-webgl');
+  const canvas = document.getElementById("canvas-webgl");
   const renderer = new THREE.WebGL1Renderer({
     antialias: false,
     canvas: canvas,
   });
-  const renderBack = new THREE.WebGLRenderTarget(document.body.clientWidth, window.innerHeight);
+  const renderBack = new THREE.WebGLRenderTarget(
+    document.body.clientWidth,
+    window.innerHeight
+  );
   const scene = new THREE.Scene();
   const sceneBack = new THREE.Scene();
   const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
-  const cameraBack = new THREE.PerspectiveCamera(45, document.body.clientWidth / window.innerHeight, 1, 10000);
+  const cameraBack = new THREE.PerspectiveCamera(
+    45,
+    document.body.clientWidth / window.innerHeight,
+    1,
+    10000
+  );
   const clock = new THREE.Clock();
   const texLoader = new THREE.TextureLoader();
   const ground = new Ground();
@@ -32,19 +41,26 @@ export default function() {
   // const skyOctahedron = new SkyOctahedron();
   // const skyOctahedronShell = new SkyOctahedronShell();
   const debris = [
-     new Debris(200, -500, 200),
-     new Debris(-350, -600, -50),
-     new Debris(-150, -700, -150),
-     new Debris(-500, -900, 0),
-     new Debris(100, -1100, 250),
-     new Debris(-100, -1200, -300),
-     new Debris(150, -1500, -100),
+    new Debris(200, -500, 200),
+    new Debris(-350, -600, -50),
+    new Debris(-150, -700, -150),
+    new Debris(-500, -900, 0),
+    new Debris(100, -1100, 250),
+    new Debris(-100, -1200, -300),
+    new Debris(150, -1500, -100),
   ];
   const postEffect = new PostEffect(renderBack.texture);
   const typo = new Typo();
   let textures;
 
-  const elemIntro = document.getElementsByClassName('js-transition-intro');
+  const elemIntro = document.getElementsByClassName("js-transition-intro");
+  const registerButton = document.getElementById("register");
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const companyInput = document.getElementById("company");
+  const titleInput = document.getElementById("title");
+  const typeSelect = document.getElementById("type");
+  const successMessage = document.getElementById("success");
 
   const resizeWindow = () => {
     canvas.width = document.body.clientWidth;
@@ -54,7 +70,7 @@ export default function() {
     renderBack.setSize(document.body.clientWidth, window.innerHeight);
     renderer.setSize(document.body.clientWidth, window.innerHeight);
     postEffect.resize();
-  }
+  };
   const render = () => {
     const time = clock.getDelta();
     titleObject.render(time);
@@ -71,15 +87,19 @@ export default function() {
     typo.update(time);
     renderer.setRenderTarget(null);
     renderer.render(scene, camera);
-  }
+  };
   const renderLoop = () => {
     render();
     requestAnimationFrame(renderLoop);
-  }
+  };
   const on = () => {
-    window.addEventListener('resize', debounce(() => {
-      resizeWindow();
-    }), 1000);
+    window.addEventListener(
+      "resize",
+      debounce(() => {
+        resizeWindow();
+      }),
+      1000
+    );
 
     scrollManager.renderNext = () => {
       if (scrollManager.isValidSmooth()) {
@@ -87,14 +107,63 @@ export default function() {
       } else {
         cameraBack.position.y = scrollManager.scrollTop * -1;
       }
-    }
-  }
+    };
+  };
   const transitionOnload = () => {
     for (var i = 0; i < elemIntro.length; i++) {
       const elm = elemIntro[i];
-      elm.classList.add('is-shown');
+      elm.classList.add("is-shown");
     }
-  }
+
+    registerButton.addEventListener("click", function () {
+      var goodToGo = true;
+      // check name input
+      if (nameInput.value == "") {
+        nameInput.classList.add("is-error");
+        goodToGo = false;
+      } else {
+        nameInput.classList.remove("is-error");
+      }
+      // check email input
+      if (emailInput.value == "") {
+        emailInput.classList.add("is-error");
+        goodToGo = false;
+      } else {
+        emailInput.classList.remove("is-error");
+      }
+      // check company input
+      if (companyInput.value == "") {
+        companyInput.classList.add("is-error");
+        goodToGo = false;
+      } else {
+        companyInput.classList.remove("is-error");
+      }
+      // check title input
+      if (titleInput.value == "") {
+        titleInput.classList.add("is-error");
+        goodToGo = false;
+      } else {
+        titleInput.classList.remove("is-error");
+      }
+      // check type select
+      if (typeSelect.value == "") {
+        typeSelect.classList.add("is-error");
+        goodToGo = false;
+      } else {
+        typeSelect.classList.remove("is-error");
+      }
+      if (goodToGo) {
+        // submit registration & clear
+        successMessage.classList.add("is-shown");
+
+        nameInput.value = ""
+        emailInput.value = ""
+        companyInput.value = ""
+        titleInput.value = ""
+        typeSelect.value = ""
+      }
+    });
+  };
 
   const init = async () => {
     renderer.setSize(document.body.clientWidth, window.innerHeight);
@@ -106,7 +175,6 @@ export default function() {
 
     buildingObject.loadTexture(() => {
       sceneBack.add(buildingObject.obj);
-      transitionOnload();
     });
 
     titleObject.loadTexture(() => {
@@ -120,9 +188,9 @@ export default function() {
     });
 
     await Promise.all([
-      texLoader.loadAsync('../img/index/tex_title.png'),
-      texLoader.loadAsync('../img/sketch/easy_glitch/noise.png'),
-    ]).then(response => {
+      texLoader.loadAsync("../img/index/tex_title.png"),
+      texLoader.loadAsync("../img/sketch/easy_glitch/noise.png"),
+    ]).then((response) => {
       textures = response;
     });
 
@@ -145,6 +213,6 @@ export default function() {
     resizeWindow();
     renderLoop();
     scrollManager.start();
-  }
+  };
   init();
 }
